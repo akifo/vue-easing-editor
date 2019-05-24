4af6b543fa2975ed8f404c203fa3dae26d9603cf
akifo <akiho@bb07.net>
2019-05-25 01:31:27 +0900

    Update dist(docs) output path for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/vue-easing-editor/' : '/',
   chainWebpack: config => {
     config.resolve.extensions.prepend('.ts')
     config.module
